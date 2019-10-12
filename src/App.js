64ba85c4da2c2ad4449c5ab8c46b12f732570770
Stef Gunderson <stefanie.gunderson@highspot.com>
2019-10-12 14:16:58 -0700

    Change file names and start loader functionality.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CardList from "./components/CardList";
 import Header from "./components/Header";
+// import Filter from "./components/Filter";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Filter /> */}
       <CardList />
     </div>
   );
