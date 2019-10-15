f40481af99a0d78580d313f6ab2f2a84332cabd5
Stef Gunderson <stefanie.gunderson@highspot.com>
2019-10-14 22:25:50 -0700

    Finally got sort functionality working.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import CardList from "./components/CardList";
 import Header from "./components/Header";
-
+import ContextProvider from "./ContextProvider";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="card-container">
-        <CardList />
+    <ContextProvider>
+      <div className="App">
+        <Header />
+        <div className="card-container">
+          <CardList />
+        </div>
       </div>
-    </div>
+    </ContextProvider>
   );
 }
 
